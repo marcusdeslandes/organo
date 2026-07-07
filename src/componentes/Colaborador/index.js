4e3d4cccf703d5bdb275bc417058135c8dc9799f
imFranMorais <marcus_deslandes@hotmail.com>
2026-07-07 17:18:09 -0300

    git commit -m ":sparkles: feat: criado cards de colaboradores"

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/marcusdeslandes.png' alt='Marcus Deslandes'/>
+            </div>
+            <div className='rodape'>
+                <h4>Marcus Deslandes</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
