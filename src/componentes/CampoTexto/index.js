b4c8cdee8297ab374a498bc3be6e253c10facee7
imFranMorais <marcus_deslandes@hotmail.com>
2026-07-07 11:17:14 -0300

    git commit -m ":bug: fix: prevent default do formulario"

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,7 +4,7 @@ const CampoTexto = (props) => { //parametro da label
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <input required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
