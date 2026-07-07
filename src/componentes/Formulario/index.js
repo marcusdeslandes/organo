335df81c98c93ff43b50ca88f4842f841982b21b
imFranMorais <marcus_deslandes@hotmail.com>
2026-07-07 10:55:23 -0300

    git commit -m ":sparkles: feat: adicionado botão formulario"

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
@@ -22,6 +23,9 @@ const Formulario = () => {
         <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
         <ListaSuspensa label="Time" itens={times}/>
+        <Botao>
+          Criar card
+        </Botao>
       </form>     
     </section>
   );
