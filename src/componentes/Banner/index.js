a422d09615ec2a5bf3bf5512b483740a041bc701
imFranMorais <marcus_deslandes@hotmail.com>
2026-07-06 21:57:27 -0300

    git commit -m ":tada: Commit inicial"

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX - Parece HTML mas não é!
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da pagina Organo"/>
+        </header>
+    )
+}
+
+export default Banner
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return (
+        <div className="campo-texto">
+            <label></label>
+            <input placeholder='Digite o seu nome'/>
+        </div>
+    )
+}
+
+export default CampoTexto
